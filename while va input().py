ef7c1c7afab67dc76ef09388d25a0083df8b63f5
azizbekjb <azizbekj279@gmail.com>
2024-11-29 22:05:52 +0500

    Update while va input().py

--- while va input().py
+++ while va input().py
@@ -1,10 +1,9 @@
-While va input()
 #While sikli yordamida dasturni to'xatatish
-# print("Kiritilgan sonni kvadartini chiqaruvchi dastur")
-# savol = "Istalgan sonni kirirting "
-# savol += "(to'xtatish uchun 'exit' deb yozing): "
-# qiymat = ''
-# while(qiymat != 'exit'):
-#     qiymat = input(savol)
-#     if qiymat != 'exit':
-#         print(float(qiymat)**2)
+ print("Kiritilgan sonni kvadartini chiqaruvchi dastur")
+ savol = "Istalgan sonni kirirting "
+ savol += "(to'xtatish uchun 'exit' deb yozing): "
+ qiymat = ''
+ while(qiymat != 'exit'):
+     qiymat = input(savol)
+     if qiymat != 'exit':
+         print(float(qiymat)**2)
