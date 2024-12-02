2cc975be4046b437e7aadacd3775a7b587e4b020
azizbekjb <azizbekj279@gmail.com>
2024-12-02 23:09:03 +0500

    Update Ammaliyot6.1.4.py

--- Ammaliyot6.1.4.py
+++ Ammaliyot6.1.4.py
@@ -1,6 +1,6 @@
 #Amaliyot 6.1.4
 xabar = "Musbat son kiriting"
-_chiqish_ = "(To'xtash uchun 'exit' yoki 'quit' so'zini kiriting):"
+_chiqish_ = "(To'xtash uchun 'exit' so'zini kiriting):"
 xabar += _chiqish_
 son = ' '
 while True:
