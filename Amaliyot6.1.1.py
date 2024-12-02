a8af082fe0f282635b90b2b1044c4006c2b23663
azizbekjb <azizbekj279@gmail.com>
2024-12-02 22:19:50 +0500

    Create Amaliyot6.1.1.py

---
+++ Amaliyot6.1.1.py
@@ -0,0 +1,13 @@
+#Amaliyot 6.1.1
+xabar = "Istalgan kitob nomini kiriting"
+_chiqish_ = "(To'xtash uchun 'stop' so'zini kiriting):"
+xabar += _chiqish_
+soz = ' '
+while soz != "stop":
+    soz = input(xabar)
+    if soz == 'stop':
+        break
+#Natija:
+    # Istalgan kitob nomini kiriting(To'xtash uchun 'stop' so'zini kiriting):Ikki eshik orasi
+    # Istalgan kitob nomini kiriting(To'xtash uchun 'stop' so'zini kiriting):stop
+    # Process finished with exit code 0
