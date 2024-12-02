d5926b45aa2c1fe1f9608876a4e2b8a1cab4f92c
azizbekjb <azizbekj279@gmail.com>
2024-12-02 23:04:32 +0500

    Create Ammaliyot6.1.4.py

---
+++ Ammaliyot6.1.4.py
@@ -0,0 +1,17 @@
+#Amaliyot 6.1.4
+xabar = "Musbat son kiriting"
+_chiqish_ = "(To'xtash uchun 'exit' yoki 'quit' so'zini kiriting):"
+xabar += _chiqish_
+son = ' '
+while True:
+    son = input(xabar)
+    if son == 'exit' :
+        break
+    elif float(son) < 0:
+        continue
+    else:
+        ildiz = float(son)**(0.5)
+        print(f"{son} ning kvadrat ildizi {ildiz} ga teng!!!")
+#Natija:
+    #Musbat son kiriting(To'xtash uchun 'exit' yoki 'quit' so'zini kiriting):exit
+    #Process finished with exit code 0
