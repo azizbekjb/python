6c712dd19ace8efe90ae8e8a8e84f5ac6cc4216f
azizbekjb <azizbekj279@gmail.com>
2024-12-04 22:43:07 +0500

    Update Amaliyot7.3.2.py

--- Amaliyot7.3.2.py
+++ Amaliyot7.3.2.py
@@ -4,7 +4,15 @@ while ishora:
     x = int(input("son kiriting: "))
     y = int(input("yana bir son kiriting: "))
     try:
-        print(x, '/', y, '=', x / y)
+        print(x/y)
+        break
     except ZeroDivisionError:
-        print("nol kiritmang")
         continue
+#Natija:Xato qiymat kiritilganda ishlaydigan dastur.To'g'ri qiymatda bir marta ishlaydi
+    # son kiriting: 2
+    # yana bir son kiriting: 0
+    # son kiriting: 3
+    # yana bir son kiriting: 0
+    # son kiriting: 3
+    # yana bir son kiriting: 1
+    # 3.0
