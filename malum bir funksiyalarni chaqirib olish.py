7733dd73128573bccb7f68c2b51abc9e863fd81c
Azizbek Jabborov <azizbekj279@gmail.com>
2024-12-14 23:25:45 +0500

    Create malum bir funksiyalarni chaqirib olish.py

---
+++ malum bir funksiyalarni chaqirib olish.py
@@ -0,0 +1,11 @@
+from avto_info_mod import avto_info, info_print # moduldagi ma'lum bir funksiyalarni chaqirib olish
+avto1 =avto_info("GM", "gentra", "Qizil", 'Mexanika', 2019, 9100)
+info_print(avto1)
+#Natija:
+    # Saytimizdagi avtolar ro'yxatini shakllantiramiz.
+    # Avtomobil(lar) haqida:
+    # Ishlab chiqaruvchi: GM
+    # Modeli: GENTRA
+    # Rangi: QIZIL
+    # Ishlab chiqarilgan yili: 2019
+    # Narxi: 9100
