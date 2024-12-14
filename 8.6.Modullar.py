463c6cbfff01674d1c378c3a192052f372114ec8
Azizbek Jabborov <azizbekj279@gmail.com>
2024-12-14 23:17:32 +0500

    Create 8.6.Modullar.py

---
+++ 8.6.Modullar.py
@@ -0,0 +1,11 @@
+import avto_info_mod #avto_info_mod modulini chaqirib olish
+avto1 = avto_info_mod.avto_info("GM", "gentra", "Qizil", 'Mexanika', 2019, 9100)
+avto_info_mod.info_print(avto1)
+#Natija:
+    # Saytimizdagi avtolar ro'yxatini shakllantiramiz.
+    # Avtomobil(lar) haqida:
+    # Ishlab chiqaruvchi: GM
+    # Modeli: GENTRA
+    # Rangi: QIZIL
+    # Ishlab chiqarilgan yili: 2019
+    # Narxi: 9100
