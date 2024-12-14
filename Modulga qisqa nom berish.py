b2491f3bb3aefe8c7906c460a1c9b3a8d2fe0ddf
Azizbek Jabborov <azizbekj279@gmail.com>
2024-12-14 23:19:29 +0500

    Create Modulga qisqa nom berish.py

---
+++ Modulga qisqa nom berish.py
@@ -0,0 +1,12 @@
+#Modulga qisqa nom berish
+import avto_info_mod as aim #avto_info_mod modulini chaqirib olish va qisqa nom berish
+avto1 = aim.avto_info("GM", "gentra", "Qizil", 'Mexanika', 2019, 9100)
+aim.info_print(avto1)
+#Natija:
+    # Saytimizdagi avtolar ro'yxatini shakllantiramiz.
+    # Avtomobil(lar) haqida:
+    # Ishlab chiqaruvchi: GM
+    # Modeli: GENTRA
+    # Rangi: QIZIL
+    # Ishlab chiqarilgan yili: 2019
+    # Narxi: 9100
