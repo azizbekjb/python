e0747d5939cf837e92c62944bab4837080bca49c
Azizbek Jabborov <azizbekj279@gmail.com>
2024-12-15 17:17:37 +0500

    Create Funksiyaga qisqa nom berish.py

---
+++ Funksiyaga qisqa nom berish.py
@@ -0,0 +1,13 @@
+#Funksiyaga qisqa nom berish
+from avto_info_mod import avto_info as ainfo # moduldagi ma'lum bir funksiyaga qisqa nom berish
+from avto_info_mod import info_print as iprint
+avto1 =ainfo("GM", "gentra", "Qizil", 'Mexanika', 2019, 9100)
+iprint(avto1)
+#Natija:
+    # Saytimizdagi avtolar ro'yxatini shakllantiramiz.
+    # Avtomobil(lar) haqida:
+    # Ishlab chiqaruvchi: GM
+    # Modeli: GENTRA
+    # Rangi: QIZIL
+    # Ishlab chiqarilgan yili: 2019
+    # Narxi: 9100
