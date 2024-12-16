3acfb9ee65761b9c28212311510368df08317525
Azizbek Jabborov <azizbekj279@gmail.com>
2024-12-16 20:35:11 +0500

    Update Talaba klasi.py

--- Talaba klasi.py
+++ Talaba klasi.py
@@ -34,3 +34,42 @@ class Talaba:
     def get_age(self, yil):
         """Talabaning yoshini qaytardi"""
         return yil - self.t_yil
+print("Klass metodlari")
+for metod in dir(Talaba):
+    print(metod)
+#Natija:
+    # Klass metodlari
+    # __class__
+    # __delattr__
+    # __dict__
+    # __dir__
+    # __doc__
+    # __eq__
+    # __format__
+    # __ge__
+    # __getattribute__
+    # __getstate__
+    # __gt__
+    # __hash__
+    # __init__
+    # __init_subclass__
+    # __le__
+    # __lt__
+    # __module__
+    # __ne__
+    # __new__
+    # __reduce__
+    # __reduce_ex__
+    # __repr__
+    # __setattr__
+    # __sizeof__
+    # __str__
+    # __subclasshook__
+    # __weakref__
+    # get_age
+    # get_fullname
+    # get_info
+    # get_lastname
+    # get_name
+    # set_bosqich
+    # update_bosqich
