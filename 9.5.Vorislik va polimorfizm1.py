9f5652d59ecd61977bd5768cf24e1fc6f8dfb47f
Azizbek Jabborov <azizbekj279@gmail.com>
2024-12-19 21:45:19 +0500

    Create 9.5.Vorislik va polimorfizm1.py

---
+++ 9.5.Vorislik va polimorfizm1.py
@@ -0,0 +1,35 @@
+#9.5.Vorislik va polimorfizm
+#Voris klass yaratish
+class Shaxs:    #Super klass
+    """Shaxslar haqida ma'lumot"""
+    def __init__(self, ism, familiya, passport, tyil):
+        self.ism = ism
+        self.familiya = familiya
+        self.passport = passport
+        self.tyil = tyil
+
+    def get_info(self):
+        """Shaxs haqida ma'lumot"""
+        info = f"{self.ism} {self.familiya}. "
+        info += f"Passport: {self.passport}, {self.tyil}-yilda tug'ilgan"
+        return info
+
+    def get_age(self, yil):
+        """Shaxsning yoshini qaytaruvchi metod"""
+        return yil - self.tyil
+
+class Talaba(Shaxs):
+    """Talaba klassi"""
+    def __init__(self, ism, familiya, passport, tyil):
+        """Talaba xususiyatlari"""
+        super().__init__(ism, familiya, passport, tyil)
+shaxs1 = Shaxs("Azizbek", "Jabborov", 'AA0000000', 2006)
+print(f"{shaxs1.get_info()}. {shaxs1.get_age(2024)} yoshda")
+
+talaba1 = Talaba("Azizbek", "Jabborov", 'AA0000000', 2006)
+print(talaba1.get_info())
+print(talaba1.get_age(2024))
+#Natija:
+    #Azizbek Jabborov. Passport: AA0000000, 2006-yilda tug'ilgan. 18 yoshda
+    # Azizbek Jabborov. Passport: AA0000000, 2006-yilda tug'ilgan
+    # 18
