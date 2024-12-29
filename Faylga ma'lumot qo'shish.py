99e5e53ecec5f3f372590879f54f7594ad8e7885
Azizbek Jabborov <azizbekj279@gmail.com>
2024-12-29 20:33:56 +0500

    Create Faylga ma'lumot qo'shish.py

---
+++ Faylga ma'lumot qo'shish.py
@@ -0,0 +1,8 @@
+#Fayllar bilan ishlash
+#Faylga ma'lumot qo'shish
+filename = 'new_file.txt'
+with open(filename, 'a') as file:
+    file.write('Alijon Valiyev\n')
+    file.write('2000')
+#Natija:
+    # Process finished with exit code 0
