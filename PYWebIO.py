15e9058bc335e245f49ea9f613f881d75bd6d905
Azizbek Jabborov <azizbekj279@gmail.com>
2025-01-01 23:54:25 +0500

    Create PYWebIO.py

---
+++ PYWebIO.py
@@ -0,0 +1,12 @@
+#12.2:Tashqi kutubxonalar
+#PYWebIO
+from pywebio.input import input, FLOAT
+from pywebio.output import put_text
+from math import pi
+
+def doira():
+    r = input("Doira radiusini kiriting:", type=FLOAT)
+    yuza = pi*r**2
+    per = 2*pi*r
+    put_text(f"Doira yuzi {yuza} ga,\nperimetri {per} ga teng")
+doira()
